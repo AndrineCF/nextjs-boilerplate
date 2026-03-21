6db4f8b3ead3b0267439ff51cbcb72c6db8af48c
ANDRI <andrine.celine.flatby@gmail.com>
2026-03-21 17:23:17 +0100

    Feat: ferdig med første seksjon

--- app/page.tsx
+++ app/page.tsx
@@ -11,42 +11,38 @@ export default function Home() {
         imageSrc="/bygging.png"
         imageAlt="Grønt tak"
       />
-      <section className="flex flex-col w-full items-center px-6 py-12 min-h-screen md:min-h-[600px] md:px-20 md:py-16">
-        <div className="text-2xl font-bold md:text-3xl">Fremtidens tak er grønne</div>
-        <div className="flex flex-row">
-          <div className="items-center">
-            <Image
-              src={"/ikoner/kart.png"}
-              alt={""}
-              width={400}
-              height={400}
-              className="rounded-lg"
-              />
-              <div className="text-center">Lokal tilpasning</div>
-          </div>
-          <div className="items-center">
-            <Image
-              src={"/ikoner/plante.png"}
-              alt={""}
-              width={400}
-              height={400}
-              className="rounded-lg"
-              />
-              <div className="text-center">Lokal tilpasning</div>
+      <section className="flex flex-col w-full items-center px-6 py-16 gap-12">
+        
+        <h2 className="text-3xl font-bold">Fremtidens tak er grønne</h2>
+
+        <div className="flex flex-row justify-center gap-24">
+          
+          <div className="flex flex-col items-center gap-4">
+            <Image src="/ikoner/kart.png" alt="" width={350} height={350} />
+            <span className="text-lg">Lokal tilpasning</span>
           </div>
 
-          <div className="items-center">
-            <Image
-              src={"/ikoner/kart.png"}
-              alt={""}
-              width={400}
-              height={400}
-              className="rounded-lg"
-              />
-              <div className="text-center">Lokal tilpasning</div>
+          <div className="flex flex-col items-center gap-4">
+            <Image src="/ikoner/plante.png" alt="" width={350} height={350}  />
+            <span className="text-lg">Smarte plantevalg</span>
+          </div>
+
+          <div className="flex flex-col items-center gap-4">
+            <Image src="/ikoner/jordklode.png" alt="" width={350} height={350}  />
+            <span className="text-lg">Miljøgevinst</span>
           </div>
 
         </div>
+
+        <a href="/om-prosjektet" className="text-xl font-bold hover:text-brand-green transition-colors">
+          Les mer her →
+        </a>
+      </section>
+      
+      <section className="flex flex-col w-full items-center px-6 py-16 gap-12 bg-light-yellow">
+
+      </section>
+      <section className="flex flex-col w-full items-center px-6 py-16 gap-12">
 
       </section>
     </main>
