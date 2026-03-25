0a2c05dc9bee8d2c0f22db511c84e700b54774f0
ANDRI <andrine.celine.flatby@gmail.com>
2026-03-25 09:07:17 +0100

    Fix. package problemer som gikk conflix fikset ved å ta bort dotenv

--- controller/chatbot.mjs
+++ controller/chatbot.mjs
@@ -1,16 +1,8 @@
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-dotenv.config({ path: resolve(__dirname, '../.env') });
 
 const embeddings = new HuggingFaceInferenceEmbeddings({
-  apiKey: process.env.HUGGINGFACEHUB_API_TOKEN, // Defaults to process.env.HUGGINGFACEHUB_API_KEY
-  model: process.env.LARGE_LLM // Defaults to `BAAI/bge-base-en-v1.5` if not provided
+  apiKey: process.env.HUGGINGFACEHUB_API_TOKEN,
+  model: process.env.LARGE_LLM ?? "BAAI/bge-base-en-v1.5"
 });
 
-console.log(embeddings)
+console.log(embeddings);
